82b6e77e304ddcf801af84c3fe3489488e81572b
wengkaimin <wengkaimin@functorz.com>
2024-01-19 12:56:16 +0800

    temp

--- src/components/editor/components/Comps/Input/index.tsx
+++ src/components/editor/components/Comps/Input/index.tsx
@@ -1,6 +1,11 @@
 import { forwardRef } from "react";
-import { ComponentRenderData } from "../../../types/Components";
+import { ComponentRenderData } from "@/components/editor/types";
+import { Input as ZInput } from "zui-pro";
 
 export const Input = forwardRef<any, ComponentRenderData>((props, ref) => {
-  return <input {...props} ref={ref} />;
+  return (
+    <div {...props} ref={ref}>
+      <ZInput style={{ height: "100%", width: "100%" }} />
+    </div>
+  );
 });
