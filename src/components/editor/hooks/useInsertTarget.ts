40007c672f56ed981773cdfaf4b62d8b54dd5c46
翁恺敏 <736653759@qq.com>
2024-01-24 00:17:22 +0800

    优化插入逻辑代码

--- src/components/editor/hooks/useInsertTarget.ts
+++ src/components/editor/hooks/useInsertTarget.ts
@@ -1,13 +1,12 @@
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { forEach, indexOf, isNil, map, min } from "lodash-es";
 import { DragOrigin } from "../types";
 import { getFlexLayoutDirection, isAbsoluteOrFixed } from "../utils/layout";
 import { useDragInfo } from "./useDragInfo";
 import { useDragOverInfo } from "./useDragOverInfo";
-import { useDndContext } from "@dnd-kit/core";
 import { useGetComponentInfo } from "./useGetComponentInfo";
 import { useEditorContext } from "./useEditorContext";
-import { getDomById } from "../utils/Dom";
+import { getDomById, getDomByOverlayId } from "../utils/Dom";
 
 function genRectToDistance(pLeft: number, pTop: number, rect: DOMRect) {
   const { left: rLeft, width: rWidth, height: rHeight, top: rTop } = rect;
@@ -23,112 +22,117 @@ export function useInsertTarget() {
 
   const dragInfo = useDragInfo();
   const overInfo = useDragOverInfo();
-  const { active } = useDndContext();
+
   const { getComponentInfo } = useGetComponentInfo();
 
-  const getDragCenterRect = useCallback(() => {
-    if (!active?.rect.current.translated) {
+  const overCompChildInfo = useMemo(() => {
+    if (!overInfo?.id) {
       return null;
     }
-    const { top, height, width, left } = active.rect.current.translated;
-    return {
-      top: top + height / 2,
-      left: left + width / 2,
-    };
-  }, [active?.rect.current.translated]);
-
-  const getClosestNodeInfo = useCallback(
-    (pLeft: number, pTop: number) => {
-      if (!overInfo?.id) {
-        return null;
+    const overNodeSchema = getComponentInfo(overInfo.id);
+    const childNodesInfo: { rect: DOMRect; index: number }[] = [];
+    forEach(overNodeSchema.childsId, (id, index) => {
+      const dom = getDomById(id);
+      const isLegalDom =
+        dom?.checkVisibility() && !isAbsoluteOrFixed(dom.style);
+      if (isLegalDom) {
+        childNodesInfo.push({
+          rect: dom!.getBoundingClientRect(),
+          index,
+        });
       }
-      const overNodeSchema = getComponentInfo(overInfo.id);
-      const childNodesInfo: { rect: DOMRect; index: number }[] = [];
-      forEach(overNodeSchema.childsId, (id, index) => {
-        const dom = getDomById(id);
-        if (dom?.checkVisibility() && !isAbsoluteOrFixed(dom.style)) {
-          childNodesInfo.push({
-            rect: dom.getBoundingClientRect(),
-            index,
-          });
-        }
-      });
-      const distances = map(childNodesInfo, ({ rect }) =>
-        genRectToDistance(pLeft, pTop, rect)
-      );
+    });
+    return childNodesInfo;
+  }, [getComponentInfo, overInfo?.id]);
 
-      const minDis = min(distances);
-      const idx = indexOf(distances, minDis);
-      return childNodesInfo[idx] || null;
-    },
-    [getComponentInfo, overInfo?.id]
-  );
-
-  const checkIsInsertable = () => {
-    if (dragInfo?.from !== DragOrigin.SIDE_ADD || !overInfo) {
+  const insertable = useMemo(() => {
+    if (!dragInfo || !overInfo || dragInfo?.from !== DragOrigin.SIDE_ADD) {
       return false;
     }
-
     if (isAbsoluteOrFixed({ position: positonMode })) {
       return false;
     }
     return true;
-  };
+  }, [dragInfo, overInfo, positonMode]);
+
+  const direction = useMemo(() => {
+    if (isNil(overInfo)) {
+      return null;
+    }
+    return getFlexLayoutDirection(getDomById(overInfo.id));
+  }, [overInfo]);
+
+  const getDragCenterRect = useCallback(() => {
+    if (isNil(dragInfo)) {
+      return;
+    }
+    const dom = getDomByOverlayId(dragInfo.id);
+    if (isNil(dom)) {
+      return null;
+    }
+    const { top, height, width, left } = dom.getBoundingClientRect();
+    return {
+      top: top + height / 2,
+      left: left + width / 2,
+    };
+  }, [dragInfo]);
+
+  const getClosestCompInfo = useCallback(
+    (pLeft: number, pTop: number) => {
+      if (isNil(overCompChildInfo) || !overCompChildInfo.length) {
+        return null;
+      }
+      const distances = map(overCompChildInfo, ({ rect }) =>
+        genRectToDistance(pLeft, pTop, rect)
+      );
+      const minDis = min(distances);
+      const idx = indexOf(distances, minDis);
+      return overCompChildInfo[idx] || null;
+    },
+    [overCompChildInfo]
+  );
 
   const getInsertInfo = () => {
-    if (!checkIsInsertable()) {
+    const dragCenter = getDragCenterRect();
+    if (isNil(insertable) || isNil(dragCenter)) {
       return;
     }
-
-    const centerRect = getDragCenterRect();
-    if (isNil(centerRect)) {
-      return;
-    }
-    const insetTargetInfo = getClosestNodeInfo(centerRect.left, centerRect.top);
+    const insetTargetInfo = getClosestCompInfo(dragCenter.left, dragCenter.top);
     if (isNil(insetTargetInfo)) {
       return;
     }
-    const { rect: insertTarget, index: targetIdx } = insetTargetInfo;
-    const {
-      top: insertTop,
-      height: insertHeight,
-      left: insertLeft,
-      width: insertWdith,
-      bottom: inertBottom,
-      right: insertRight,
-    } = insertTarget;
+    const { left: centerLeft, top: centerTop } = dragCenter;
 
-    const { left: centerLeft, top: centerTop } = centerRect;
-    const targetCenterTop = insertTop + insertHeight / 2;
-    const targetCenterLeft = insertLeft + insertWdith / 2;
+    const { rect, index: targetIdx } = insetTargetInfo;
+    const { top, height, left, width, bottom, right } = rect;
 
-    const direction = getFlexLayoutDirection(getDomById(overInfo!.id));
+    const targetCenterTop = top + height / 2;
+    const targetCenterLeft = left + width / 2;
+
+    const insertRect = { ...rect };
+    let insertIdx = targetIdx;
 
     if (direction === "vertical") {
-      const isNearTop = centerTop > targetCenterTop;
-      const insertRect = {
-        ...insertTarget,
-        left: targetCenterLeft,
-        top: isNearTop ? inertBottom : insertTop,
-      };
-      return {
-        insertIdx: isNearTop ? targetIdx + 1 : targetIdx,
-        insertRect,
-        direction,
-      };
+      insertRect.left = targetCenterLeft;
+      insertRect.top = top;
+      if (centerTop > targetCenterTop) {
+        insertRect.top = bottom;
+        insertIdx = targetIdx + 1;
+      }
     } else if (direction === "horizontal") {
-      const isNearLeft = centerLeft > targetCenterLeft;
-      const insertRect = {
-        ...insertTarget,
-        top: targetCenterTop,
-        left: isNearLeft ? insertRight : insertLeft,
-      };
-      return {
-        insertIdx: isNearLeft ? targetIdx + 1 : targetIdx,
-        insertRect,
-        direction,
-      };
+      insertRect.top = targetCenterTop;
+      insertRect.left = left;
+      if (centerLeft > targetCenterLeft) {
+        insertRect.left = right;
+        insertIdx = targetIdx + 1;
+      }
     }
+
+    return {
+      insertIdx,
+      direction,
+      insertRect,
+    };
   };
 
   return getInsertInfo;
